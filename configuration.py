0fb7d50a03f83ea588a1cdd448a50727c9c4b163
Maria Elena Noriega <mariaenc1970@gmail.com>
2026-07-27 23:04:39 +0100

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-b98d08ca-a480-44b5-9a9b-8ffcf8d9b118.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
